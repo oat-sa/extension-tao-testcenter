d643b4072d8d05f46d985008b8561dae873a0503
Alexander Zagovorichev <zagovorichev@gmail.com>
2017-04-27 11:45:02 +0300

    fixed updater

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -22,7 +22,6 @@
 
 namespace oat\taoTestCenter\scripts\update;
 
-use oat\tao\scripts\update\OntologyUpdater;
 /**
  *
  * @access public
@@ -41,6 +40,6 @@ class Updater extends \common_ext_ExtensionUpdater
             throw new \common_Exception('Upgrade unavailable');
         }
 
-        $this->skip('0', '2.0.0');
+        $this->skip('0.3.0', '2.0.0');
     }
 }
