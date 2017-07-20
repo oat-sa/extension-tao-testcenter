8de5e574022ff90a89733e8f8f6643ad356e884d
Aleksej Tikhanovich <aleksej@taotesting.com>
2017-07-20 13:56:27 +0300

    Updated manifest version

--- manifest.php
+++ manifest.php
@@ -34,10 +34,11 @@ return array(
     'label' => 'Test Center',
     'description' => 'Proctoring via test-centers',
     'license' => 'GPL-2.0',
-    'version' => '2.0.3',
+    'version' => '2.0.4',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
-        'taoProctoring' => '>=4.4.2'
+        'taoProctoring' => '>=4.4.2',
+        'taoDelivery' => '>=7.0.0'
     ),
     'managementRole' => TestCenterService::ROLE_TESTCENTER_MANAGER,
     'acl' => array(
