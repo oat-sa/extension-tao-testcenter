fd8843855e6eb4bdf54413d77d90a8e4b692d48b
Mikhail Kamarouski <kamarouski@1pt.com>
2018-04-19 10:42:45 +0300

    Initial interfaces for csv imports

---
+++ config/default/testCenterCsvImporterFactory.conf.php
@@ -0,0 +1,14 @@
+в<?php
+/**
+ * Default config header created during install
+ */
+
+return new \oat\taoTestCenter\model\import\TestCenterCsvImporterFactory([
+    'mappers' => [],
+    'default-schema' => [
+        'mandatory' => [
+            'label' => 'http://www.w3.org/2000/01/rdf-schema#label',
+        ],
+        'optional' => []
+    ]
+]);
