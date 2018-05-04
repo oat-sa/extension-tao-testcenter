58e8e2e58d33c043243f10d5e92da2f8c5f2bf50
Aleh Hutnikau <hutnikau@1pt.com>
2018-05-04 12:13:00 +0300

    bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -160,6 +160,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('3.10.0');
         }
 
-        $this->skip('3.10.0', '3.10.1');
+        $this->skip('3.10.0', '3.10.2');
     }
 }
