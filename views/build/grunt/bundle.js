0ab14e085c76b6c2b7bbcb744f4ace74e41c83f9
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2018-10-07 15:40:41 +0200

    migrate to atomic bundles

--- views/build/grunt/bundle.js
+++ views/build/grunt/bundle.js
@@ -1,62 +1,45 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2014-2018 (original work) Open Assessment Technologies SA;
+ */
+
+/**
+ * configure the extension bundles
+ * @author Bertrand Chevrier <bertrand@taotesting.com>
+ */
 module.exports = function(grunt) {
+    'use strict';
 
-    var requirejs   = grunt.config('requirejs') || {};
-    var clean       = grunt.config('clean') || {};
-    var copy        = grunt.config('copy') || {};
-
-    var root        = grunt.option('root');
-    var libs        = grunt.option('mainlibs');
-    var ext         = require(root + '/tao/views/build/tasks/helpers/extensions')(grunt, root);
-    var out         = 'output';
-    
-    var paths = {
-        'taoTestCenter'          : root + '/taoTestCenter/views/js',
-        'taoTestCenterCss'       : root + '/taoTestCenter/views/css',
-        'taoProctoring'          : root + '/taoProctoring/views/js',
-        'taoProctoringCss'       : root + '/taoProctoring/views/css',
-        'taoClientDiagnostic'    : root + '/taoClientDiagnostic/views/js',
-        'taoClientDiagnosticCss' : root + '/taoClientDiagnostic/views/css',
-        'taoQtiItem'             : root + '/taoQtiItem/views/js',
-        'taoItems'               : root + '/taoItems/views/js',
-        'taoQtiTest'             : root + '/taoQtiTest/views/js'
-    };
-    
-    /**
-     * Remove bundled and bundling files
-     */
-    clean.taotestcenterbundle = [out];
-
-    /**
-     * Compile tao files into a bundle
-     */
-    requirejs.taotestcenterbundle = {
-        options: {
-            baseUrl : '../js',
-            dir : out,
-            mainConfigFile : './config/requirejs.build.js',
-            paths : paths,
-            modules : [{
-                name: 'taoTestCenter/controller/routes',
-                include : ext.getExtensionsControllers(['taoProctoring']),
-                exclude : ['mathJax'].concat(libs)
-            }]
+    grunt.config.merge({
+        bundle : {
+            taotestcenter : {
+                options : {
+                    extension : 'taoTestCenter',
+                    outputDir : 'loader',
+                    dependencies : ['taoProctoring'],
+                    bundles : [{
+                        name : 'taoTestCenter',
+                        default : true,
+                        include : ['taoTestCenter/component/**/*']
+                    }]
+                }
+            }
         }
-    };
-
-    /**
-     * copy the bundles to the right place
-     */
-    copy.taotestcenterbundle = {
-        files: [
-            { src: [out + '/taoTestCenter/controller/routes.js'],  dest: root + '/taoTestCenter/views/js/controllers.min.js' },
-            { src: [out + '/taoTestCenter/controller/routes.js.map'],  dest: root + '/taoTestCenter/views/js/controllers.min.js.map' }
-        ]
-    };
-
-    grunt.config('clean', clean);
-    grunt.config('requirejs', requirejs);
-    grunt.config('copy', copy);
+    });
 
     // bundle task
-    grunt.registerTask('taotestcenterbundle', ['clean:taotestcenterbundle', 'requirejs:taotestcenterbundle', 'copy:taotestcenterbundle']);
+    grunt.registerTask('taotestcenterbundle', ['bundle:taotestcenter']);
 };
