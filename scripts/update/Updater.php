7c2d92073e07b3d7b995edca22d168b7f2590d5c
Ivan Klimchuk <ivan@klimchuk.com>
2019-02-21 12:54:50 +0300

    Fix updater

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -399,7 +399,9 @@ class Updater extends common_ext_ExtensionUpdater
                 )
             );
 
-            $this->setVersion('4.8.0');
+            $this->setVersion('4.7.0');
         }
+
+        $this->skip('4.7.0', '4.8.0');
     }
 }
