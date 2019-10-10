4b0cd7bab59ff84edd3e5bf107216abca91a0894
Razhkou <yuryr@taotesting.com>
2019-10-10 12:30:50 +0300

    TTD-18 Added\changed 'generis' version to '>=12.5.0' in `requires` secition

--- manifest.php
+++ manifest.php
@@ -47,7 +47,7 @@ return array(
     'requires' => array(
         'taoProctoring'  => '>=12.7.0',
         'taoDelivery'    => '>=12.5.0',
-        'generis'        => '>=11.2.3',
+        'generis'        => '>=12.5.0',
         'tao'            => '>=35.5.1',
         'taoTestTaker'   => '>=4.0.0',
         'taoDeliveryRdf' => '>=6.0.0',
