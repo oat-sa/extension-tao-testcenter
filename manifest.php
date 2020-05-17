19354c8df6e5e4b7c267fb93787d0495776f5bae
uncleempty <ilya@taotesting.com>
2020-05-17 17:59:29 +0300

    Backport version bump

--- manifest.php
+++ manifest.php
@@ -42,7 +42,7 @@ return array(
     'label' => 'Test Center',
     'description' => 'Proctoring via test-centers',
     'license' => 'GPL-2.0',
-    'version' => '8.2.1',
+    'version' => '8.2.1.1',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'taoProctoring'  => '>=12.7.0',
