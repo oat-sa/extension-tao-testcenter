9ede080170fb70a3efd0093821f4beef8934c09b
Camille Moyon <camille@taotesting.com>
2023-11-15 18:06:18 +0100

    fix: do not allow non exisiting deliveries to be evaluated

--- model/TestCenterAssignment.php
+++ model/TestCenterAssignment.php
@@ -1,21 +1,21 @@
 <?php
-/**  
+/**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
  * of the License (non-upgradable).
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- * 
+ *
  * Copyright (c) 2015 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
- * 
+ *
  */
 namespace oat\taoTestCenter\model;
 
@@ -138,7 +138,7 @@ class TestCenterAssignment extends GroupAssignment
         foreach ($user->getPropertyValues(self::PROPERTY_TESTTAKER_ASSIGNED) as $eligibilityId) {
             $eligibility = $this->getResource($eligibilityId);
             $eligibilityDelivery = $eligibility->getOnePropertyValue($deliveryProp);
-            if ($delivery->equals($eligibilityDelivery) && $delivery->exists()) {
+            if (null !== $eligibilityDelivery && $delivery->equals($eligibilityDelivery) && $delivery->exists()) {
                 $eligibilityService = $this->getServiceManager()->get(EligibilityService::SERVICE_ID);
                 if ($eligibilityService->isDeliveryEligible($delivery, $user)) {
                     return true;
